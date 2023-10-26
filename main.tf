1a0f30564068a1510ca328f2ad9d71b55e210a9f
AgnerLoss <agner1984@yahoo.com.br>
2023-10-26 00:37:37 -0300

    update skip provider

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  #skip_provider_registration = true 
+  skip_provider_registration = true 
   features {}  
-  subscription_id = "3ec358c5-ecbe-411f-94e6-bea82052d9c0" 
+  #subscription_id = "3ec358c5-ecbe-411f-94e6-bea82052d9c0" 
 }
 
 # Create a resource group
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network" "vnet-projeto" {
   name                = "vnet-projeto"
   resource_group_name = azurerm_resource_group.rg_projeto.name
   location            = azurerm_resource_group.rg_projeto.location
-  address_space       = ["10.1.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
 }
 
 
