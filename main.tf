8ffb982e3bfb68c122c319bd86ca00103c6bf347
AgnerLoss <agner1984@yahoo.com.br>
2023-10-25 23:10:58 -0300

    update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,9 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {} 
+  features {}
+  subscription_id = "3ec358c5-ecbe-411f-94e6-bea82052d9c0" 
+  tenant_id       = "67096ccc-f018-4ecb-a0ff-caaa7bf467e2"
 }
 
 # Create a resource group
