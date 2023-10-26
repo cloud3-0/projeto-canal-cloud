1c491c5b30c5bcc9bbc38908a654131174c39ef4
AgnerLoss <agner1984@yahoo.com.br>
2023-10-25 21:33:25 -0300

    Teste inicial projeto cloud

--- main.tf
+++ main.tf
@@ -11,9 +11,8 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
-  subscription_id = "caa30e72-9633-45ce-bd6d-66d8300e4a6b"
-  tenant_id       = "1a5b6d50-9253-413e-b103-fdf9df5fabf1" 
+  skip_provider_registration = true 
+  features {}    
 }
 
 # Create a resource group
