003459da19242f7084534af370c554264bc47928
Lucas Pampolha <lpampolhas@gmail.com>
2023-10-26 21:44:49 -0300

    Adição do bloco de Backend

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = ">= 3.74.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstateRG01"
+    storage_account_name = "<storage_account_name>"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 # Configure the Microsoft Azure Provider
