906436da639ffaa78e3d2f4965ad63472ba500d4
Lucas Pampolha <lpampolhas@gmail.com>
2023-11-10 13:56:50 -0300

    Remote Backend

--- main.tf
+++ main.tf
@@ -6,24 +6,47 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "tfstateRG01"
-    storage_account_name = "<storage_account_name>"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  skip_provider_registration = true 
   features {}   
 }
 
 # Create a resource group
 resource "azurerm_resource_group" "rg_projeto" {
   name     = "rg_projeto"
-  location = "West Europe"
+  location = "East US"
+}
+
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate${random_string.resource_code.result}"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
 }
 
 # Create a virtual network within the resource group
@@ -33,5 +56,3 @@ resource "azurerm_virtual_network" "vnet-projeto" {
   location            = azurerm_resource_group.rg_projeto.location
   address_space       = ["10.0.0.0/16"]
 }
-
-
