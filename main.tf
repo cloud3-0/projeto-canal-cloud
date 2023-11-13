e3f87bede81838aa1776a7a95fc62f3c2e3d3939
Lucas Pampolha <lpampolhas@gmail.com>
2023-11-13 12:46:55 -0300

    Separação do arquivo Backend

--- main.tf
+++ main.tf
@@ -10,12 +10,14 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+  # skip_provider_registration = true
   features {}   
 }
 
 # Create a resource group
 resource "azurerm_resource_group" "rg_projeto" {
   name     = "rg_projeto"
+  # location = "West Europe"
   location = "East US"
 }
 
@@ -23,30 +25,6 @@ resource "random_string" "resource_code" {
   length  = 5
   special = false
   upper   = false
-}
-
-resource "azurerm_resource_group" "tfstate" {
-  name     = "tfstate"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.resource_code.result}"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = azurerm_resource_group.tfstate.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  allow_nested_items_to_be_public = false
-
-  tags = {
-    environment = "staging"
-  }
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
 }
 
 # Create a virtual network within the resource group
