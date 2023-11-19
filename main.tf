5c56f8f2652d7d4099b8eeb641da4dc38f9cd712
AgnerLoss <agner1984@yahoo.com.br>
2023-11-19 16:52:46 -0300

    Atualizar subscription

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  skip_provider_registration = true 
-  features {}   
+  skip_provider_registration = true
+  features {}
 }
 
 # Create a resource group
